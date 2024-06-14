529d0d6c29929b9ea81090a2c8baa5a33d26bbd0
Aman Verma <95334005+AMAN1011011@users.noreply.github.com>
2024-06-14 19:29:12 +0530

    Add files via upload

---
+++ Armstrong Numbers.cpp
@@ -0,0 +1,24 @@
+//Driver Code
+
+class Solution {
+  public:
+    string armstrongNumber(int n) {
+        // code here
+        int z = n;
+        
+        int a = z%10;
+        z = z/10;
+        int b = z%10;
+        z = z /10;
+        int c = z%10;
+        
+        int ans = a*a*a + b*b*b + c*c*c;
+        if(ans==n){
+            return "true";
+        }
+        else
+        return "false";
+    }
+};
+
+//Driver code
