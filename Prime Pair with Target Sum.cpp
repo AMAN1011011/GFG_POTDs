ba593f3f5516f01dfee44da6a6f3b05e7d023c3b
Aman Verma <95334005+AMAN1011011@users.noreply.github.com>
2024-06-16 09:55:28 +0530

    Add files via upload

---
+++ Prime Pair with Target Sum.cpp
@@ -0,0 +1,24 @@
+//Driver Code
+class Solution {
+  public:
+    vector<int> getPrimes(int n) {
+        // code here
+        vector<bool> isPrime(n+2 , true);
+        isPrime[0] = false;
+        isPrime[1] = false;
+        for(int i = 2 ; i<n ;i++){
+            if(isPrime[i]){
+                for(int j = 2 ; i*j<n ;j++){
+                    isPrime[i*j]=false;
+                }
+            }
+        }
+        for(int i = 2 ; i<n; i++){
+            if(isPrime[i] && isPrime[n-i]){
+                return {i , n-i};
+            }
+        }
+        return {-1,-1};
+    }
+};
+//Driver code
