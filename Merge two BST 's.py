f9a1964f773bef65f4ccd3526014d3546b5c1634
Aman Verma <95334005+AMAN1011011@users.noreply.github.com>
2024-09-30 21:48:44 +0530

    Add files via upload

---
+++ Merge two BST 's.py
@@ -0,0 +1,14 @@
+class Solution:
+    def merge(self, root1, root2):
+        # code here
+        l=[]
+        def tree(root,l):
+            if root is None:
+                return 
+            l.append(root.data)
+            tree(root.left,l)
+            tree(root.right,l)
+        tree(root1,l)
+        tree(root2,l)
+        l.sort()
+        return l
